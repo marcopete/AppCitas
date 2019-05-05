0fc2aaba0417d1083687b1c686567c4a45ad7a7c
unknown <marco.puga@gmail.com>
2019-05-05 01:27:41 -0400

    Primer Commit - Despliegue de datos de bd

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext //El DataContext hereda DbContext de EntityFramework
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet <Value> Values { get; set; } //Nombre de tabla en sql
+    }
+}
