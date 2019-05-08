deaef490c38128ed02a1d24bac2ea9d94f4f43ef
unknown <marco.puga@gmail.com>
2019-05-07 23:04:27 -0400

    Fin de seccion de autenticacion

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -11,16 +17,17 @@ namespace DatingApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            _config = config;
             _repo = repo;
         }
 
+        // Llamado a "funcion" de registro en controlador de autorizacion
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            //validar el request
-
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if (await _repo.UserExists(userForRegisterDto.Username))
@@ -32,10 +39,48 @@ namespace DatingApp.API.Controllers
                 Username = userForRegisterDto.Username
             };
 
-            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);    
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
             //return CreatedAtRoute()
+        }
+        // Llamado a "funcion" de login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            //Funcion encoding requiere using System.Text;
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+               .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            //credenciales
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            //Descripcion del token, funcion DateTime requiere Sysem
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+            //JwtSecurityTokenHandler requiere System.IdentityModel.Tokens.Jwt;
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        //Propiedad de campo que requiere valor, largo minimo de campo
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "tu contraseña debe ser entre 4 y 8")]
         public string Password { get; set; }
     }
 }
